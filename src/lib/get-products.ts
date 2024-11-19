fb50f53788e09164a884dde2daeae07aea7996ac
CristianLeyton <leytoncristian96@gmail.com>
2024-11-18 22:08:56 -0300

    Filtro por categorias

--- src/lib/get-products.ts
+++ src/lib/get-products.ts
@@ -1,14 +1,26 @@
 import { query } from "./connection";
-const {STRAPI_HOST} = import.meta.env
+const { STRAPI_HOST } = import.meta.env;
 
 export function getAllProducts() {
-    return query('products?fields[0]=name&fields[1]=description&fields[2]=id_product&fields[3]=price&populate[image][fields][1]=url')
-    .then(res => {
+  return query(
+    "products?fields[0]=name&fields[1]=description&fields[2]=id_product&fields[3]=price&populate[image][fields][1]=url"
+  ).then((res) => {
+    res.data.forEach((element) => {
+      element.image.url = `${STRAPI_HOST}${element.image.url}`;
+    });
 
-        res.data.forEach(element => {
-            element.image.url = `${STRAPI_HOST}${element.image.url}` 
-        });
+    return res.data;
+  });
+}
 
-        return res.data
-    })
+export function getProducts(id_categoria) {
+  return query(
+    `products?fields[0]=name&fields[1]=description&fields[2]=id_product&fields[3]=price&populate[image][fields][1]=url&populate[categorie][fields][1]=id_categorie&filters[categorie][id_categorie][$eq]=${id_categoria}`
+  ).then((res) => {
+    res.data.forEach((element) => {
+      element.image.url = `${STRAPI_HOST}${element.image.url}`;
+    });
+
+    return res.data;
+  });
 }
