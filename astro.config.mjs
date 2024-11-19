106e9fc9f3b4806a40c0af02374b6ca75b069acd
CristianLeyton <leytoncristian96@gmail.com>
2024-11-18 21:06:42 -0300

    test 2 vercel edploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,6 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server', // Necesario para Vercel
-  adapter: vercel(),
 });
