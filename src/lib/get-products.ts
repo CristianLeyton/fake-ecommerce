3080d8518daea0fc26da4c4db2562fb9b0f7c645
Cristian Leyton <112524960+CristianLeyton@users.noreply.github.com>
2024-11-22 14:35:41 -0300

    Actualizar get-products.ts

--- src/lib/get-products.ts
+++ src/lib/get-products.ts
@@ -3,7 +3,7 @@ const { STRAPI_HOST } = import.meta.env;
 
 export function getAllProducts() {
   return query(
-    "products?fields[0]=name&fields[1]=description&fields[2]=id_product&fields[3]=price&populate[image][fields][1]=url"
+    "products?fields[0]=name&fields[1]=description&fields[2]=id_product&fields[3]=price&fields[4]=sku&populate[image][fields][1]=url"
   ).then((res) => {
     res.data.forEach((element) => {
       element.image.url = `${STRAPI_HOST}${element.image.url}`;
@@ -15,7 +15,7 @@ export function getAllProducts() {
 
 export function getProducts(id_categoria) {
   return query(
-    `products?fields[0]=name&fields[1]=description&fields[2]=id_product&fields[3]=price&populate[image][fields][1]=url&populate[categorie][fields][1]=id_categorie&filters[categorie][id_categorie][$eq]=${id_categoria}`
+    `products?fields[0]=name&fields[1]=description&fields[2]=id_product&fields[3]=price&fields[4]=sku&populate[image][fields][1]=url&populate[categorie][fields][1]=id_categorie&filters[categorie][id_categorie][$eq]=${id_categoria}`
   ).then((res) => {
     res.data.forEach((element) => {
       element.image.url = `${STRAPI_HOST}${element.image.url}`;
