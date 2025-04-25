bf295e839c0e78aded7e5feff213b518839b4825
CristianLeyton <leytoncristian96@gmail.com>
2025-04-25 16:00:52 -0300

    cache laravel

--- src/lib/connection.ts
+++ src/lib/connection.ts
@@ -1,29 +1,15 @@
 const { API_URL } = import.meta.env;
 
-// Cache para almacenar las respuestas
-const cache = new Map();
-const CACHE_DURATION = 5 * 1000; // 5 segundos
 
 export function query(url: string) {
-  const now = Date.now();
-  const cacheKey = `${url}-${Math.floor(now / CACHE_DURATION)}`;
-
-  // Si tenemos una respuesta en caché y no ha expirado, la devolvemos
-  if (cache.has(cacheKey)) {
-    return Promise.resolve(cache.get(cacheKey));
-  }
-
-  // Si no hay caché o ha expirado, hacemos la petición
+  // Hacemos la petición sin caché del lado del cliente
   return fetch(`${API_URL}/api/${url}`, {
     headers: {
-      "Cache-Control": "no-cache",
+      "Cache-Control": "no-cache, no-store, must-revalidate",
+      Pragma: "no-cache",
+      Expires: "0",
     },
   })
     .then((res) => res.json())
-    .then((res) => {
-      const data = res.data;
-      // Guardamos en caché
-      cache.set(cacheKey, data);
-      return data;
-    });
+    .then((res) => res.data);
 }
