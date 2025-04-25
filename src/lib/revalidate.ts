f9868313b7c59e232f31af10f31f3c409f3cf32f
CristianLeyton <leytoncristian96@gmail.com>
2025-04-25 15:45:32 -0300

    test

--- src/lib/revalidate.ts
+++ src/lib/revalidate.ts
@@ -1,14 +1,3 @@
 export async function revalidate() {
-  if (import.meta.env.DEV) return;
-
-  const response = await fetch("/api/revalidate", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-
-  if (!response.ok) {
-    console.error("Error al revalidar");
-  }
+  return; // Desactiva la revalidación
 }
