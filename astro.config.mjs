de2887d4c131b203feec3f339dfca7184ab3ca5d
CristianLeyton <leytoncristian96@gmail.com>
2025-04-25 23:17:47 -0300

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
-import compress from 'astro-compress';
 
 export default defineConfig({
   site: 'https://fireworks.com',
   output: 'static',
   integrations: [
     tailwind(),
-    react(),
     sitemap(),
-    compress({
-      CSS: true,
-      HTML: {
-        removeAttributeQuotes: false,
-        removeOptionalTags: false,
-      },
-      Image: false,
-      JavaScript: true,
-      SVG: false,
-    }),
   ],
   vite: {
     build: {
